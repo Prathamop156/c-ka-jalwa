8849d2d202594f30c3f6dd3a9da9ddb7d609cf5a
Prathamop156 <160272001+Prathamop156@users.noreply.github.com>
2024-08-13 18:52:58 +0530

    Add files via upload

---
+++ 2nd.c
@@ -0,0 +1,23 @@
+// area of rectangle
+#include <stdio.h>
+
+int main(){
+
+    int length, breadth;
+    printf("enter length\n");
+    scanf("%d" , &length);
+    printf("enter breadth\n");
+    scanf("%d" , &breadth);
+
+    printf("the area of rectangle is %d",length*breadth);
+    
+
+
+
+
+
+    return 0;
+}
+
+
+
