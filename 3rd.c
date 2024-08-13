0ffac6f4bebd09ce7ea9ccf7e15f37aa0ef64354
Prathamop156 <160272001+Prathamop156@users.noreply.github.com>
2024-08-13 18:53:50 +0530

    Add files via upload

---
+++ 3rd.c
@@ -0,0 +1,12 @@
+//area of circle and volume of cylinder 
+
+#include<stdio.h>
+
+int main(){
+    int r = 6;
+    int h = 10;
+    printf("the area of cicle with radius%d is %f\n",r,3.14*r*r);
+    printf("the volume of cylinder with radius %d and height %d is %f",r,h,3.14*r*r*h);
+    return 0;
+    
+}
