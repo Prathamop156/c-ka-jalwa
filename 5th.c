3eadb5cf17ea2746e7b9ba876578ea9c209c227f
Prathamop156 <160272001+Prathamop156@users.noreply.github.com>
2024-08-13 18:55:29 +0530

    Add files via upload

---
+++ 5th.c
@@ -0,0 +1,16 @@
+// simple interest calc
+
+#include<stdio.h>
+
+
+
+
+int main(){
+    float p = 33.3;
+    int r = 7;
+    int t = 8;
+    printf("the value of simple interest is %f",(p*r*t)/100);
+    return 0;
+
+
+}
